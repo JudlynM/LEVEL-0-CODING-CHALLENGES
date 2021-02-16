f7d45e5381717f5ac4f87b14ef7a5734c8d7b5fa
Judlyn <59104652+JudlynM@users.noreply.github.com>
2021-02-16 22:30:36 +0200

    Update Task10.js

--- Task10.js
+++ Task10.js
@@ -4,15 +4,16 @@ function commonCharacters(string1 , string2) {
     var temp = [];
 
 
-    for(var i=0; i < string1.length; i++) 
-    {
+    for(var i=0; i < string1.length; i++){
+        
       output[string1.charAt(i)] = 1;
     }  
-    for(var i=0; i < string2.length; i++) 
-    {
-      if( output[string2.charAt(i)] == 1) {
+    
+    for(var i=0; i < string2.length; i++){     
+      if( output[string2.charAt(i)] == 1){
+          
           temp.push(string2.charAt(i));
-      }
+      }    
     }
     
     output = temp.join(" ");
