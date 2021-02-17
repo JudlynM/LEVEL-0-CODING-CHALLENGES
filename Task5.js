4351d10ca0d69c4d8434d31d244ec08762c9766a
Judlyn <59104652+JudlynM@users.noreply.github.com>
2021-02-17 14:23:27 +0200

    Update Task5.js

--- Task5.js
+++ Task5.js
@@ -5,12 +5,8 @@ It calculates the area of a triangle when you know the lengths of all three side
 The formula is given by:  Area = Sqrt[p(p-x1)(p-x2)(p-x3)] where p = (x1 + x2 + x3)/2 i.e. half of perimeter
 */
 
-
 function areaOfTriangle(x,y,z){
-
     var p = ((x+y+z)/2);
-
     return Math.sqrt((p)*(p-x)*(p-y)*(p-z));
 }
-
 console.log(areaOfTriangle(20,5,19));
