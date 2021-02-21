ab83e3804f3794f97cd7e711e4134147cb7638f0
Judlyn <59104652+JudlynM@users.noreply.github.com>
2021-02-21 14:36:06 +0200

    Update Task10.js

--- Task10.js
+++ Task10.js
@@ -9,15 +9,17 @@ function commonCharacters(string1 , string2) {
       output[string1.charAt(i)] = 1;
     }  
     
-    for(var i=0; i < string2.length; i++){     
-      if( output[string2.charAt(i)] == 1){
+    for(var j=0; j < string2.length; j++){     
+      if( output[string2.charAt(j)] === 1){
           
-          temp.push(string2.charAt(i));
+          temp.push(string2.charAt(j));
       }    
     }
     
-    output = temp.join(", ");
-    console.log("Common letters: " + output);
+    output = temp.join("");
+    var noDuplicates = Array.from(new Set(output));
+    console.log("Common letters: " + noDuplicates.toString());
+    
   }
   
   commonCharacters("house", "computers");
