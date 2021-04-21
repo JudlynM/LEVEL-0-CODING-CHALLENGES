e3639b73bbfec257f859677d3dba79f622ef1edc
Judlyn <59104652+JudlynM@users.noreply.github.com>
2021-04-21 02:56:07 +0200

    Update Task8.js

--- Task8.js
+++ Task8.js
@@ -1,23 +1,19 @@
-function timeConverter( value ){
-      
-      var hoursConvert = Math.floor(value/60);
-      var minutesConvert = value % 60;
+function timeConverter(value) {
+	let hoursConvert = Math.floor(value / 60);
+	let minutesConvert = value % 60;
 
-      hours = hoursConvert.toString();
-      minutes = minutesConvert.toString();
+	hours = hoursConvert.toString();
+	minutes = minutesConvert.toString();
 
-        if((hoursConvert === 1) && (minutesConvert === 1)){
-        return hours + " hour, " + minutes + " minute";
-        }
-        else if((hoursConvert === 1)){
-        return hours + " hour, " + minutes + " minutes";
-        }
-        else if(minutesConvert === 1){
-        return hours + " hours, " + minutes + " minute"; 
-        }
-        else{
-        return hours + " hours, " + minutes + " minutes";
-        }
+	if (hoursConvert === 1 && minutesConvert === 1) {
+		return hours + " hour, " + minutes + " minute";
+	} else if (hoursConvert === 1) {
+		return hours + " hour, " + minutes + " minutes";
+	} else if (minutesConvert === 1) {
+		return hours + " hours, " + minutes + " minute";
+	} else {
+		return hours + " hours, " + minutes + " minutes";
+	}
 }
 
-console.log(timeConverter(71));
+console.log(timeConverter(61));
