5710e854ab4b67671903749406111fdc8b9f74c9
Judlyn <59104652+JudlynM@users.noreply.github.com>
2021-04-21 03:04:22 +0200

    Update Task9.js

--- Task9.js
+++ Task9.js
@@ -1,58 +1,54 @@
-function printVowels( input ){
+function printVowels(input) {
+	let output = [];
 
-        var output = [ ]; 
+	for (i = 0; i < input.length; i++) {
+		switch (input[i]) {
+			case "a":
+				output.push(input[i]);
+				break;
 
-    for(i=0 ; i < input.length; i++) 
-    {
+			case "e":
+				output.push(input[i]);
+				break;
 
-        switch(input[i])
-        {
-            case "a":
-                output.push(input[i]);
-                break;
+			case "i":
+				output.push(input[i]);
+				break;
 
-            case "e":
-                output.push(input[i]);
-                break;
+			case "o":
+				output.push(input[i]);
+				break;
 
-            case "i":
-                output.push(input[i]);
-                break;
+			case "u":
+				output.push(input[i]);
+				break;
 
-            case "o":
-                output.push(input[i]);
-                break;
+			case "A":
+				output.push(input[i]);
+				break;
 
-            case "u":
-                output.push(input[i]);
-                break;
-            
-           case "A":
-                output.push(input[i]);
-                break;
-    
-            case "E":
-                output.push(input[i]);
-                break;
-    
-            case "I":
-                output.push(input[i]);
-                break;
-    
-            case "O":
-                output.push(input[i]);
-                break;
-    
-            case "U":
-                output.push(input[i]);
-                break;
-                
-        }
+			case "E":
+				output.push(input[i]);
+				break;
 
-    }
-    return output.toString();
+			case "I":
+				output.push(input[i]);
+				break;
 
+			case "O":
+				output.push(input[i]);
+				break;
+
+			case "U":
+				output.push(input[i]);
+				break;
+		}
+	}
+	return "Vowels: " + output.toString();
 }
 
-
-console.log(printVowels("Hello world, my name is Judlyn (Jude) and I am enjoying Javascript!"));
+console.log(
+	printVowels(
+		"Hello world, my name is Judlyn (Jude) and I am enjoying Javascript!"
+	)
+);
